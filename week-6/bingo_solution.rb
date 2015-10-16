9ae8c54835bed1c07a5ab4d21fa51b497899c270
Isaac Lee <message.isaac.lee@gmail.com>
2015-10-15 21:48:20 -0700

    finish bingo board

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -23,6 +23,55 @@
 
 # Initial Solution
 
+# class BingoBoard
+
+#   def initialize(board)
+#     @bingo_board = board
+#     @announce = nil
+#     @letters = nil
+#     @numbers = nil
+#   end
+
+#   # def roll (letter, number)
+#   #   @letters = letter
+#   #   @numbers = number
+#   # end
+
+
+#   def play
+#     @letters = ["B", "I", "N", "G", "O"].sample
+#     @numbers = 1 + rand(100)
+#     # print @bingo_board[1][0]
+#     if @letters == "B"
+#       @bingo_board.each {|i| i[0] = " X" if i[0] == @numbers}
+#     elsif @letters == "I"
+#       @bingo_board.each {|i| i[1] = " X" if i[1] == @numbers}
+#     elsif @letters == "N"
+#       @bingo_board.each {|i| i[2] = " X" if i[2] == @numbers}
+#     elsif @letters == "G"
+#       @bingo_board.each {|i| i[3] = " X" if i[3] == @numbers}
+#     elsif @letters == "O"
+#       @bingo_board.each {|i| i[4] = " X" if i[4] == @numbers}
+#     end
+#   end
+
+#   def show_board
+#     print " | " + @bingo_board[0][0].to_s + " | " + @bingo_board[0][1].to_s + " | " + @bingo_board[0][2].to_s + " | " + @bingo_board[0][3].to_s + " | " + @bingo_board[0][4].to_s + " |\n"
+#     print " | " + @bingo_board[1][0].to_s + " | " + @bingo_board[1][1].to_s + " | " + @bingo_board[1][2].to_s + " | " + @bingo_board[1][3].to_s + " | " + @bingo_board[1][4].to_s + " |\n"
+#     print " | " + @bingo_board[2][0].to_s + " | " + @bingo_board[2][1].to_s + " | " + @bingo_board[2][2].to_s + " | " + @bingo_board[2][3].to_s + " | " + @bingo_board[2][4].to_s + " |\n"
+#     print " | " + @bingo_board[3][0].to_s + " | " + @bingo_board[3][1].to_s + " | " + @bingo_board[3][2].to_s + " | " + @bingo_board[3][3].to_s + " | " + @bingo_board[3][4].to_s + " |\n"
+#     print " | " + @bingo_board[4][0].to_s + " | " + @bingo_board[4][1].to_s + " | " + @bingo_board[4][2].to_s + " | " + @bingo_board[4][3].to_s + " | " + @bingo_board[4][4].to_s + " |\n"
+#   end
+
+#   def last_roll
+#     print @letters
+#     print @numbers
+#   end
+
+# end
+
+# Refactored Solution
+
 class BingoBoard
 
   def initialize(board)
@@ -32,9 +81,10 @@ class BingoBoard
     @numbers = nil
   end
 
-  # def roll
-  #   @letters = ["B", "I", "N", "G", "O"].rand
-  #   @numbers = 1 + rand(100)
+  # def roll (letter, number)
+  #   @letters = letter
+  #   @numbers = number
+  # end
 
 
   def play
@@ -42,20 +92,24 @@ class BingoBoard
     @numbers = 1 + rand(100)
     # print @bingo_board[1][0]
     if @letters == "B"
-      @bingo_board.each{|i| @bingo_board[i][0] = "X" if @bingo_board[i][0] == @numbers}
+      @bingo_board.each {|i| i[0] = " X" if i[0] == @numbers}
     elsif @letters == "I"
-      @bingo_board.each{|i| @bingo_board[i][1] = "X" if @bingo_board[i][1] == @numbers}
+      @bingo_board.each {|i| i[1] = " X" if i[1] == @numbers}
     elsif @letters == "N"
-      @bingo_board.each{|i| @bingo_board[i][2] = "X" if @bingo_board[i][2] == @numbers}
+      @bingo_board.each {|i| i[2] = " X" if i[2] == @numbers}
     elsif @letters == "G"
-      @bingo_board.each{|i| @bingo_board[i][3] = "X" if @bingo_board[i][3] == @numbers}
+      @bingo_board.each {|i| i[3] = " X" if i[3] == @numbers}
     elsif @letters == "O"
-      @bingo_board.each{|i| @bingo_board[i][4] = "X" if @bingo_board[i][4] == @numbers}
+      @bingo_board.each {|i| i[4] = " X" if i[4] == @numbers}
     end
   end
 
   def show_board
-    print @bingo_board
+    print " | " + @bingo_board[0][0].to_s + " | " + @bingo_board[0][1].to_s + " | " + @bingo_board[0][2].to_s + " | " + @bingo_board[0][3].to_s + " | " + @bingo_board[0][4].to_s + " |\n"
+    print " | " + @bingo_board[1][0].to_s + " | " + @bingo_board[1][1].to_s + " | " + @bingo_board[1][2].to_s + " | " + @bingo_board[1][3].to_s + " | " + @bingo_board[1][4].to_s + " |\n"
+    print " | " + @bingo_board[2][0].to_s + " | " + @bingo_board[2][1].to_s + " | " + @bingo_board[2][2].to_s + " | " + @bingo_board[2][3].to_s + " | " + @bingo_board[2][4].to_s + " |\n"
+    print " | " + @bingo_board[3][0].to_s + " | " + @bingo_board[3][1].to_s + " | " + @bingo_board[3][2].to_s + " | " + @bingo_board[3][3].to_s + " | " + @bingo_board[3][4].to_s + " |\n"
+    print " | " + @bingo_board[4][0].to_s + " | " + @bingo_board[4][1].to_s + " | " + @bingo_board[4][2].to_s + " | " + @bingo_board[4][3].to_s + " | " + @bingo_board[4][4].to_s + " |\n"
   end
 
   def last_roll
@@ -64,9 +118,6 @@ class BingoBoard
   end
 
 end
-
-# Refactored Solution
-
 
 
 #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
@@ -80,12 +131,12 @@ board = [[47, 44, 71, 8, 88],
 new_game = BingoBoard.new(board)
 new_game.show_board
 new_game.play
-new_game.last_roll
 new_game.play
 new_game.play
 new_game.play
 new_game.play
 new_game.show_board
+new_game.last_roll
 
 
 #Reflection
