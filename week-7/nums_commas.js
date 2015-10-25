8a6f21fcb6e48acf81b69f2bdff9bb304a0f1ce2
Isaac Lee <message.isaac.lee@gmail.com>
2015-10-24 21:40:54 -0700

    finish group project part 3

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -1,7 +1,8 @@
+
 // Separate Numbers with Commas in JavaScript **Pairing Challenge**
 
 
-// I worked on this challenge with: .
+// I worked on this challenge with: Un Choi.
 
 // Pseudocode
 // 1. Turn the number into a string
@@ -10,23 +11,58 @@
 
 
 // Initial Solution
-function seperateComma (number);
-	if String(number).length < 3;
-		return number;
-	else
-		
+// function separateComma (number){
+// // Turn the number into a string.
+// var string_number = String(number);
+// //   console.log(string_number);
+//   if (string_number.length <= 3)
+//     return number;
+//   else 
+//     var reverse_number = string_number.split("").reverse().join("");
+//     var number_with_commas = reverse_number
+//     for (var position = 0; position < reverse_number.length; position++)
+//       if ((position+1) % 4 == 0 && position != 0) 
+//         var number_with_commas = [number_with_commas.slice(0, position), ",", number_with_commas.slice(position)].join("");
+//     var final_number = number_with_commas.split("").reverse().join("");
+//        return final_number
+// }
+  
+//     console.log(string_number);
+//     var output = [a.slice(0, position), b, a.slice(position)].join('');
+//     for (var count = 0; count < exponent; count++)
+
+
+// console.log(string_number);
+
+//   else
+    
 
 
 
 
 // Refactored Solution
+function reverse(string) {
+  return string.split("").reverse().join("");
+}
 
-
+function separateComma (number) {
+var string_number = String(number);
+  if (string_number.length <= 3)
+    return number;
+  else 
+    var reverse_number = reverse(string_number);
+    var number_with_commas = reverse_number;
+    for (var position = 0; position < reverse_number.length; position++)
+      if ((position+1) % 4 == 0 && position != 0) 
+        var number_with_commas = [number_with_commas.slice(0, position), ",", number_with_commas.slice(position)].join("");
+    var final_number = reverse(number_with_commas);
+       return(final_number);
+}
 
 
 // Your Own Tests (OPTIONAL)
 
-separateComma(1569743)
+// separateComma(12345678)
 
 
 // Reflection
